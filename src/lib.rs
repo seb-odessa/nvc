df3277454b4ccd2ff821b3d42fab45f0be67ccd2
Pavel Kolievatykh <pavlo.kolievatykh@dxc.com>
2022-08-15 17:06:27 +0300

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -39,18 +39,37 @@ impl Nvc {
                     return nvc.next(tail);
                 }
             }
+        } else {
+            return self.branches();
         }
         return Vec::new();
     }
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
-mod nvc {
+mod tests {
     use super::*;
 
     #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
+    #[test]
     fn init() {
-        assert!(Nvc::new().branches().is_empty());
+        assert!(!Nvc::new().branches().is_empty());
     }
 
     #[test]
@@ -75,7 +94,10 @@ mod nvc {
         nvc.insert("abcu");
         nvc.insert("acub");
 
-        assert_eq!(nvc.next("z"), vec![]);
+        assert_eq!(false, true);
+
+        // assert_eq!(nvc.next(""), vec!['a', 'b']);
+        assert_eq!(nvc.next("z"), vec!['a']);
         assert_eq!(nvc.next("a"), vec!['b', 'c']);
         assert_eq!(nvc.next("ab"), vec!['c', 'r']);
         assert_eq!(nvc.next("abr"), vec!['a', 'e', 'u']);
@@ -83,5 +105,32 @@ mod nvc {
         assert_eq!(nvc.next("ac"), vec!['u']);
         assert_eq!(nvc.next("acu"), vec!['b']);
         assert_eq!(nvc.next("abc"), vec!['a', 'u']);
+    }
+
+    #[test]
+    fn search_nvc() {
+        let lines = vec![
+            "Заря над Араксом",
+            "Заря над Араксом",
+            "Заря над бездной",
+            "Заря над бухтой",
+            "Заря над Сибирью",
+            "Заря над Скаргиаром",
+            "Заря над степью",
+            "Заря над Уссури",
+        ];
+        let mut nvc = Nvc::new();
+        for line in &lines {
+            nvc.insert(line);
+        }
+
+        for line in &lines {
+            let mut path = String::with_capacity(line.len());
+            let chars = line.chars();
+            while let Some(ch) = chars.next() {
+
+            }
+
+        }
     }
 }
